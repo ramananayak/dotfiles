bac9f302e6eb3b324e686ce7551cc2f90c4cbe40
Ramanath Nayak <65753883+ramananayak@users.noreply.github.com>
2026-08-10 22:17:19 +0530

    fix: markdown-preview plugin removed

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -11,7 +11,7 @@ vim.pack.add({
     { src = "https://github.com/tpope/vim-fugitive" },
     { src = "https://github.com/nvim-lua/plenary.nvim" },
     { src = "https://github.com/olimorris/codecompanion.nvim" },
-    { src = "https://github.com/MeanderingProgrammer/render-markdown.nvim" },
+    { src = "https://github.com/MeanderingProgrammer/render-markdown.nvim" }
 })
 
 require("nvim-web-devicons").setup()
